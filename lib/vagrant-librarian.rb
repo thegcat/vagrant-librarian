2bf35da187e3b19e0a591c551c72801d2725d57c
VASMAN <vasman@hidev.it>
2013-02-07 16:44:48 +0200

    Also run on reload and provision

--- lib/vagrant-librarian.rb
+++ lib/vagrant-librarian.rb
@@ -2,3 +2,5 @@ require "vagrant-librarian/version"
 require "vagrant-librarian/install"
 
 Vagrant.actions[:up].insert_before Vagrant::Action::VM::Provision, Vagrant::Librarian::Install
+Vagrant.actions[:reload].insert_before Vagrant::Action::VM::Provision, Vagrant::Librarian::Install
+Vagrant.actions[:provision].insert_before Vagrant::Action::VM::Provision, Vagrant::Librarian::Install
